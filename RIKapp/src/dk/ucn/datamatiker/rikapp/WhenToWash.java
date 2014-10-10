2d091c1038678d6b36422d010df557342c1d5b24
cimeren <kim.nyegaard@gmail.com>
2014-10-10 16:44:11 +0200

    Info - DONE!!!!!

---
+++ RIKapp/src/dk/ucn/datamatiker/rikapp/WhenToWash.java
@@ -0,0 +1,15 @@
+package dk.ucn.datamatiker.rikapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class WhenToWash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_when_to_wash);
+	}
+}
