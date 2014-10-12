5a33ad340867d08207e80d2667283998387af8c3
Jesper Andersen <zuggie@streamcoins.com>
2014-10-12 18:55:59 +0200

    no message

--- RIKapp/src/dk/ucn/datamatiker/rikapp/Video.java
+++ RIKapp/src/dk/ucn/datamatiker/rikapp/Video.java
@@ -2,7 +2,10 @@ package dk.ucn.datamatiker.rikapp;
 
 import dk.ucn.datamatiker.rikapp.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -11,10 +14,22 @@ public class Video extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_avoid_infection);
+		setContentView(R.layout.activity_video);
 		
 		VideoView vid = (VideoView) findViewById(R.id.stepVideo);
-		vid.setVideoPath("/res/raw/rik_film.MP4");
+		vid.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.rik_film);
 		vid.setMediaController(new MediaController(this));
+		vid.requestFocus();
+		vid.pause();
+		
+		Button mainmenubtn = (Button)findViewById(R.id.main_menu_btn);
+		mainmenubtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Video.this, RIKappMainActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
