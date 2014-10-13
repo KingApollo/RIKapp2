2ec3d0f2265638e69dc8a4020608938c41cbdb5e
Jesper Andersen <zuggie@streamcoins.com>
2014-10-13 08:18:52 +0200

    no message

--- RIKapp/src/dk/ucn/datamatiker/rikapp/Video.java
+++ RIKapp/src/dk/ucn/datamatiker/rikapp/Video.java
@@ -2,10 +2,7 @@ package dk.ucn.datamatiker.rikapp;
 
 import dk.ucn.datamatiker.rikapp.R;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
